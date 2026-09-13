3914c3ae2ae64bd715ff6839edbad9d41f31941d
Angela Maharjan <angelamaharjanwork0610@gmail.com>
2026-09-13 08:16:20 +0545

    add file

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cineview</title>
+    <link rel = "stylesheet" href="css/style.css">
+</head>
+<body>
+   <header>
+      <nav class="navbar">
+        <div class="logo">
+            CineView
+        </div>
+        <ul class="nav-links">
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="login.php">Login</a>
+            </li>
+            <li>
+                <a href="register.php" class="register.btn">Register</a>
+            </li>
+
+        </ul>
+    </nav>
+</header>
+<section class="hero">
+    <div class="hero-content">
+        <h1>
+            Discover Your Next<span>Movie</span>
+        </h1>
+        <p>Read.Rate.Review</p>
+        <a href="movies.php"class="explore-btn">Explore Movies</a>
+    </div>
+        <div class="hero-image></div>
+    </section>
+</body>
+</html>
